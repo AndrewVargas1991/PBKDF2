3890ae89c13ed500fa49572808915bec6d723469
Andrew Vargas <andrewmvargas@yahoo.com.br>
2024-07-06 19:36:26 -0300

    Add files via upload

---
+++ PBKDF2.py
@@ -0,0 +1,15 @@
+import hashlib
+
+interacoes = 100000
+comprimento_chave = 32
+senha = input('Digite sua senha: ').encode('utf-8')
+sal = input('Digite seu sal: ').encode('utf-8')
+
+# Se quiser gerar o sal de maneira aleatória, descomente as linhas abaixo
+#   e salve o sal para usar futuramente
+# sal = os.urandom(16)
+
+hash_com_sal = hashlib.pbkdf2_hmac('sha256', senha, sal, interacoes, comprimento_chave)
+print(f'PBKDF2 de SHA256: {hash_com_sal.hex()}')
+
+input('\nAperte ENTER para sair...')
